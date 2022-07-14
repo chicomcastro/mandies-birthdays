da988475bf0c199635798501657f46323d9cd3f8
Francisco Castro <francisco.castro@sooper.com.br>
2022-07-14 14:34:09 -0300

    feat: add confetti lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import confetti from "canvas-confetti";
+import { useEffect } from "react";
 
 function App() {
+  const fireworkBurst = () => {
+    var count = 200;
+    var defaults = {
+      origin: { y: 0.7 },
+    };
+
+    function fire(particleRatio, opts) {
+      confetti(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(count * particleRatio),
+        })
+      );
+    }
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    fire(0.2, {
+      spread: 60,
+    });
+
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  };
+
+  useEffect(() => {
+    fireworkBurst();
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" onClick={() => fireworkBurst()}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
